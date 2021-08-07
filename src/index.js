8dbdf66c83a3abac8eddc0393eec312f2a749662
ksuryas <surya.kasibhatla@gmail.com>
2021-08-07 14:49:37 +0530

    made it pwa app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import { AppProvider } from './context/context';
 import { SpeechProvider } from '@speechly/react-client';
-import registerServiceWorker from 'react-service-worker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const application = (
   <SpeechProvider appId="d215319f-3a39-4cb7-b8c2-d45627349b24" language="en-US">
@@ -15,4 +15,8 @@ const application = (
 );
 
 ReactDOM.render(application, document.getElementById('root'));
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorkerRegistration.register();
